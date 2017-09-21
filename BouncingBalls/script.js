cc5b56c96473dd1cf04b0b4514207f71e1c0015c
erla97 <erlaos207@nemi.tskoli.is>
2017-09-21 23:32:17 +0000

    Bouncing Balls

---
+++ BouncingBalls/script.js
@@ -0,0 +1,123 @@
+// setup canvas
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function til að búa til random tölu
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
+
+//Búið er til smið fyrir boltana
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+//Prototype sem teiknar boltana
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+//bolti til að testa virkni
+var testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+
+testBall.x
+testBall.size
+testBall.color
+testBall.draw()
+
+//Uppfærir staðsetningu boltanna
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+//bolta fylki
+var balls = [];
+
+//function sem birtir svartan bakgrunn og litar boltana í ýmsum litum og setur þá í ýmsar stærðir,
+//birtir þá og finnur staðsetningar þeirra
+function loop() {
+  ctx.fillStyle = 'rgba(25, 25, 25, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 250) {
+    var ball = new Ball(
+      random(0,width),
+      random(0,height),
+      random(-7,7),
+      random(-7,7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+      random(10,20)
+    );
+    balls.push(ball);
+  }
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+//Kallað á fallið loop
+loop();
+
+//Tékkar hvort bolti hittir á eitthvað og lætur hann breyta um lit
+Ball.prototype.collisionDetect = function() {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
+//Kallar á fallið
+balls[i].collisionDetect();
+
+var _this = this;
+window.onkeydown = function(e) {
+    if (e.keyCode === 65) {
+      _this.x -= _this.velX;
+    } else if (e.keyCode === 68) {
+      _this.x += _this.velX;
+    } else if (e.keyCode === 87) {
+      _this.y -= _this.velY;
+    } else if (e.keyCode === 83) {
+      _this.y += _this.velY;
+    }
+}
